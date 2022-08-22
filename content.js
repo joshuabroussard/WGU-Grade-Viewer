81176c80947065928961d50ceecc6db9a8f57749
Joshua Broussard <joshuabroussard1996@gmail.com>
2022-08-22 18:36:01 -0500

    Updated for new WGU assessment HTML/CSS

--- content.js
+++ content.js
@@ -5,7 +5,7 @@ function main(evt) {
     let scores = 0;
     var timer = setInterval(runsOnJSLoadFinish, 111);
     function runsOnJSLoadFinish () {
-        scores = document.getElementsByClassName("grid margin--bottom--20 padding--left--15 padding--right--15")
+        scores = document.getElementsByClassName("score-bar");
         if (scores.length != 0) {
             clearInterval(timer);
             for(let i = 0; i < scores.length; i++) {
@@ -16,9 +16,18 @@ function main(evt) {
 
 }
 
-function placeScore(score) {
-    // within the given score element find the span with class "margin--left--5" and find with width of the div with class "assessment__progress-bar__progress assessment__progress-bar__progress_-exemplary"
-    const span = score.getElementsByClassName("margin--left--5");
-    const width = score.getElementsByClassName("assessment__progress-bar__progress assessment__progress-bar__progress")[0].style.width;
-    span[0].innerHTML = span[0].innerHTML + " " + width;
+function placeScore(parent) {
+    // within the given score element find the span with class "margin--left--5" and find with width of the div with class 
+    // "assessment__progress-bar__progress assessment__progress-bar__progress"
+    console.log(parent)
+
+    let right = parent.getElementsByClassName("value")[0].style.right;
+    //parse the % out of the string
+    let percent = parseInt(right.substring(0, right.length - 1));
+
+    //add the score value into the parent container
+    currentHtml = parent.innerHTML;
+    // span should be 2 rem font size
+    parent.innerHTML = `<span style='font-size:2rem'>${100-Number(percent)}%</span>`+ currentHtml;
+
 }
